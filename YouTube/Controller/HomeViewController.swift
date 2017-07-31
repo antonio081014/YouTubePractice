3e937671b231bc4cda88a25b6443f3880df1d46d
Antonio081014 <antonio081014@antonio081014.com>
2017-07-31 14:31:19 -0700

    Reorganize project into MVC grouped structure.

---
+++ YouTube/Controller/HomeViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by Antonio081014 on 7/30/17.
+//  Copyright © 2017 Antonio081014.com. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "Home"
+        self.navigationController?.navigationBar.isTranslucent = false
+        
+        let titleLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width - 32, height: self.view.bounds.height))
+        titleLabel.text = "Home"
+        titleLabel.textColor = .white
+        titleLabel.font = UIFont.systemFont(ofSize: 20)
+        self.navigationItem.titleView = titleLabel
+        
+        self.collectionView?.backgroundColor = UIColor.white
+        
+        self.collectionView?.register(VideoCell.self, forCellWithReuseIdentifier: "cellID")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = self.view.bounds.width
+        let height = (width - 16 - 16) * 9 / 16
+        return CGSize(width: width, height: height + 16 + 68)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
