3c041a2a97492047a026dc43f631381086a00bb9
Antonio081014 <antonio081014@antonio081014.com>
2017-07-30 21:14:29 -0700

    Initial Commit

---
+++ YouTube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by Antonio081014 on 7/30/17.
+//  Copyright © 2017 Antonio081014.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
