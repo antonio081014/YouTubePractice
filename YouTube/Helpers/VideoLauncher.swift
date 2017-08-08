f43b4e06733020304f5bd008fd0058755b8aa920
Antonio081014 <antonio081014@antonio081014.com>
2017-08-07 17:21:19 -0700

    Trying to enable play/pause function for video player.

--- YouTube/Helpers/VideoLauncher.swift
+++ YouTube/Helpers/VideoLauncher.swift
@@ -11,10 +11,61 @@ import AVFoundation
 
 class VideoPlayerView: UIView {
     
+    let activityIndicatorView: UIActivityIndicatorView = {
+        let aiv = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.whiteLarge)
+        aiv.translatesAutoresizingMaskIntoConstraints = false
+        aiv.hidesWhenStopped = true
+        return aiv
+    }()
+    
+    lazy var pauseButton: UIButton = {
+        let button = UIButton(type: .system)
+        // TODO: Add play/pause image to assets.
+        let image = UIImage(named: "")
+        button.setImage(image, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = .white
+        
+        button.addTarget(self, action: #selector(handlePause), for: .touchUpInside)
+        return button
+    }()
+    
+    let controlsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 1)
+        return view
+    }()
+    
+    @objc func handlePause() {
+        
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        self.setupPlayerView()
+        
+        self.controlsContainerView.frame = frame
+        self.addSubview(self.controlsContainerView)
+        
+        self.controlsContainerView.addSubview(self.activityIndicatorView)
+        self.activityIndicatorView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        self.activityIndicatorView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        
+        self.controlsContainerView.addSubview(self.pauseButton)
+        self.pauseButton.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        self.pauseButton.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        self.pauseButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        self.pauseButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        
         self.backgroundColor = .black
         
+        
+    }
+    
+    private func setupPlayerView() {
+        // TODO: add a playable video link.
         let urlString = ""
         if let url = URL(string: urlString) {
             let player = AVPlayer(url: url)
@@ -22,8 +73,16 @@ class VideoPlayerView: UIView {
             self.layer.addSublayer(playerLayer)
             playerLayer.frame = self.frame
             player.play()
+            
+            player.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
         }
-        
+    }
+    
+    override func addObserver(_ observer: NSObject, forKeyPath keyPath: String, options: NSKeyValueObservingOptions = [], context: UnsafeMutableRawPointer?) {
+        if keyPath == "currentItem.loadedTimeRanges" {
+            self.activityIndicatorView.stopAnimating()
+            self.controlsContainerView.backgroundColor = .clear
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
